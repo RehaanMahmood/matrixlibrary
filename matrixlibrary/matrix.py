10d447d4738d08366c6fdb2ca72039eaa3f0ab79
Rehaan Mahmood <rehaanmd.310506@gmail.com>
2025-07-20 13:31:02 +0400

    modified:   matrixlibrary/__init__.py modified:   matrixlibrary/matrix.py deleted:    matrixlibrary/utils.py

--- matrixlibrary/matrix.py
+++ matrixlibrary/matrix.py
@@ -1,6 +1,3 @@
-from .utils import identity
-
-
 det_cache: dict = {}
 
 
@@ -267,3 +264,45 @@ class Matrix:
         reduced = aug_matrix.rref()
         inv = [row[self.nrows :] for row in reduced.matrix]
         return Matrix(inv)
+
+
+def zeros(r: int, c: int, as_matrix: bool = True) -> Matrix | list[list[int]]:
+    output: list[list[int]] = []
+    for i in range(r):
+        row: list[int] = []
+        for j in range(c):
+            row.append(0)
+        output.append(row)
+    return Matrix(output) if as_matrix else output
+
+
+def ones(r: int, c: int, as_matrix: bool = True) -> Matrix | list[list[int]]:
+    output: list[list[int]] = []
+    for i in range(r):
+        row: list[int] = []
+        for j in range(c):
+            row.append(1)
+        output.append(row)
+    return Matrix(output) if as_matrix else output
+
+
+def diag(
+        seq: list[int | float | complex] | tuple[int | float | complex],
+        as_matrix: bool = True,
+) -> Matrix | list[list[int | float | complex]]:
+    n: int = len(seq)
+    output: list[list[int]] = zeros(n, n, as_matrix=False)
+    for i in range(n):
+        output[i][i] = seq[i]
+    return Matrix(output) if as_matrix else output
+
+
+def identity(
+        n: int, as_matrix: bool = True
+) -> Matrix | list[list[int | float | complex]]:
+    return diag([1] * n, as_matrix=as_matrix)
+
+
+def display(matrix: list[list[int | float | complex]]) -> None:
+    for row in matrix:
+        print(row)
